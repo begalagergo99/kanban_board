9f53f641f7c19f7060f718284466c3b87b644ccf
begalagergo99 <begala.g1999@gmail.com>
2024-04-06 16:34:25 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Kanbanboard } from './screens/KanbanBoard'
+
+function App() {
+  return (
+    <>
+   <Kanbanboard />
+    </>
+  )
+}
+
+export default App
---
+++ src/screens/KanbanBoard.tsx
@@ -0,0 +1,11 @@
+export const Kanbanboard = () => {
+
+  return (
+    <div>
+      <h1>Kanban Board</h1>
+      <div className="board">
+        
+      </div>
+    </div>
+  );
+}
